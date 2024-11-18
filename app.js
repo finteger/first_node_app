6cfd3f9f6812a0377056e730b66e548aadadcb6a
finteger <nashassociatesinc@gmail.com>
2024-11-18 11:21:59 -0700

    Update

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-//in the browser http://localhost:3000/user/23
+//Middleware
+app.use(express.json());
+app.use((req, res, next) =>{
+   console.log(`This works first`);
+   next();
+});
+
+app.get('/', (req, res) =>{
+ console.log("This works second");
+});
 
 app.get("/user/:id", (req, res) => {
    const userId = req.params.id;
